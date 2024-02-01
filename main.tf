03e42f836bb5fe37111f11721c842a6b564455b0
hyacinth <hieu.b.dao@vntrip.vn>
2024-02-01 13:55:19 +0700

    update terraform

--- main.tf
+++ main.tf
@@ -14,19 +14,19 @@ provider "aws" {
   region  = var.REGION
 }
 
-resource "aws_vpc" "project-vpc" {
+resource "aws_vpc" "cicd-vpc" {
   cidr_block = var.VPC_CIDR
   instance_tenancy = "default"
   enable_dns_hostnames = true
   enable_dns_support = true
 
   tags = {
-    Name = "project-vpc"
+    Name = "cicd-vpc"
   }
 }
 
 resource "aws_subnet" "public-subnet-01" {
-  vpc_id = aws_vpc.project-vpc.id
+  vpc_id = aws_vpc.cicd-vpc.id
   availability_zone = var.AZ_PUBLIC_01
   cidr_block = var.PUBLIC_CIDR_01
 
@@ -36,7 +36,7 @@ resource "aws_subnet" "public-subnet-01" {
 }
 
 resource "aws_subnet" "public-subnet-02" {
-  vpc_id = aws_vpc.project-vpc.id
+  vpc_id = aws_vpc.cicd-vpc.id
   availability_zone = var.AZ_PUBLIC_02
   cidr_block = var.PUBLIC_CIDR_02
 
@@ -46,7 +46,7 @@ resource "aws_subnet" "public-subnet-02" {
 }
 
 resource "aws_internet_gateway" "project-igw" {
-  vpc_id = aws_vpc.project-vpc.id
+  vpc_id = aws_vpc.cicd-vpc.id
 
   tags = {
     Name = "project-igw"
@@ -54,7 +54,7 @@ resource "aws_internet_gateway" "project-igw" {
 }
 
 resource "aws_route_table" "project-public-rtb" {
-  vpc_id = aws_vpc.project-vpc.id
+  vpc_id = aws_vpc.cicd-vpc.id
 
   route {
     cidr_block = var.ANYWHERE_CIDR
@@ -76,10 +76,10 @@ resource "aws_route_table_association" "public-rtb-associate-subnet-02" {
   route_table_id = aws_route_table.project-public-rtb.id
 }
 
-resource "aws_security_group" "gs-sg" {
-  name = "gs-sg"
-  description = "Security group for GS"
-  vpc_id = aws_vpc.project-vpc.id
+resource "aws_security_group" "gitlab-sg" {
+  name = "gitlab-sg"
+  description = "Security group for Gitlab server"
+  vpc_id = aws_vpc.cicd-vpc.id
 
   ingress {
     from_port = 8090
@@ -110,14 +110,14 @@ resource "aws_security_group" "gs-sg" {
   }
 
   tags = {
-    Name = "gs-sg"
+    Name = "gitlab-sg"
   }
 }
 
-resource "aws_security_group" "gr-sg" {
-  name = "gr-sg"
-  description = "Security group for GR"
-  vpc_id = aws_vpc.project-vpc.id
+resource "aws_security_group" "runner-sg" {
+  name = "runner-sg"
+  description = "Security group for Runner & Registry"
+  vpc_id = aws_vpc.cicd-vpc.id
 
   ingress {
     from_port = 5000
@@ -148,14 +148,14 @@ resource "aws_security_group" "gr-sg" {
   }
 
   tags = {
-    Name = "gr-sg"
+    Name = "runner-sg"
   }
 }
 
-resource "aws_security_group" "be-sg" {
-  name = "be-sg"
-  description = "Security group for BE"
-  vpc_id = aws_vpc.project-vpc.id
+resource "aws_security_group" "backend-sg" {
+  name = "backend-sg"
+  description = "Security group for Backend Web APIs"
+  vpc_id = aws_vpc.cicd-vpc.id
 
   ingress {
     from_port = 3000
@@ -186,7 +186,7 @@ resource "aws_security_group" "be-sg" {
   }
 
   tags = {
-    Name = "be-sg"
+    Name = "backend-sg"
   }
 }
 
@@ -195,8 +195,8 @@ resource "aws_security_group_rule" "inbound_ssh_gr" {
   from_port                = 22
   to_port                  = 22
   protocol                 = "tcp"
-  source_security_group_id = aws_security_group.be-sg.id
-  security_group_id        = aws_security_group.gr-sg.id
+  source_security_group_id = aws_security_group.backend-sg.id
+  security_group_id        = aws_security_group.runner-sg.id
 }
 
 resource "aws_security_group_rule" "inbound_ssh_be" {
@@ -204,95 +204,95 @@ resource "aws_security_group_rule" "inbound_ssh_be" {
   from_port                = 22
   to_port                  = 22
   protocol                 = "tcp"
-  source_security_group_id = aws_security_group.gr-sg.id
-  security_group_id        = aws_security_group.be-sg.id
+  source_security_group_id = aws_security_group.runner-sg.id
+  security_group_id        = aws_security_group.backend-sg.id
 }
 
-resource "aws_instance" "gs-instance" {
-  ami = var.DEFAULT_AMI
-  instance_type = var.PROVIDER_INSTANCE_TYPE
-  key_name = var.KEY_PAIR
+# resource "aws_instance" "gitlab-instance" {
+#   ami = var.DEFAULT_AMI
+#   instance_type = var.PROVIDER_INSTANCE_TYPE
+#   key_name = var.KEY_PAIR
 
-  subnet_id = aws_subnet.public-subnet-01.id  
-  vpc_security_group_ids = [aws_security_group.gs-sg.id]
-  associate_public_ip_address = true
+#   subnet_id = aws_subnet.public-subnet-01.id  
+#   vpc_security_group_ids = [aws_security_group.gitlab-sg.id]
+#   associate_public_ip_address = true
 
-  user_data = <<-EOF
-    #!/bin/bash
-    apt-get update
-    apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-    apt-get update
-    apt-get install docker-ce docker-ce-cli containerd.io -y
-    curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-    chmod +x /usr/local/bin/docker-compose
-    mkdir -p /home/app/gitlab
-  EOF
+#   user_data = <<-EOF
+#     #!/bin/bash
+#     apt-get update
+#     apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
+#     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+#     add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+#     apt-get update
+#     apt-get install docker-ce docker-ce-cli containerd.io -y
+#     curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+#     chmod +x /usr/local/bin/docker-compose
+#     mkdir -p /home/app/gitlab
+#   EOF
 
-  tags = {
-    Name = "gs-instance"
-  }
-}
+#   tags = {
+#     Name = "gitlab-instance"
+#   }
+# }
 
-resource "aws_instance" "gr-instance" {
-  ami = var.DEFAULT_AMI
-  instance_type = var.PROVIDER_INSTANCE_TYPE
-  key_name = var.KEY_PAIR
+# resource "aws_instance" "runner-instance" {
+#   ami = var.DEFAULT_AMI
+#   instance_type = var.PROVIDER_INSTANCE_TYPE
+#   key_name = var.KEY_PAIR
 
-  subnet_id = aws_subnet.public-subnet-01.id  
-  vpc_security_group_ids = [aws_security_group.gr-sg.id]
-  associate_public_ip_address = true
+#   subnet_id = aws_subnet.public-subnet-01.id  
+#   vpc_security_group_ids = [aws_security_group.runner-sg.id]
+#   associate_public_ip_address = true
 
-  user_data = <<-EOF
-    #!/bin/bash
-    apt-get update
-    apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y 
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-    apt-get update
-    apt-get install docker-ce docker-ce-cli containerd.io -y
-    curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-    chmod +x /usr/local/bin/docker-compose
-    curl -L "https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh" | bash
-    apt-get install gitlab-runner -y
-    apt-cache madison gitlab-runner
-    gitlab-runner -version
-    usermod -aG docker gitlab-runner
-    usermod -aG sudo gitlab-runner
-    mkdir -p /home/docker/registry && chmod -R 777 /home/docker/registry && cd /home/docker/registry 
-    mkdir certs data
-    apt get update
-    apt-get install openssl -y
-  EOF
+#   user_data = <<-EOF
+#     #!/bin/bash
+#     apt-get update
+#     apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y 
+#     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+#     add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+#     apt-get update
+#     apt-get install docker-ce docker-ce-cli containerd.io -y
+#     curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+#     chmod +x /usr/local/bin/docker-compose
+#     curl -L "https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh" | bash
+#     apt-get install gitlab-runner -y
+#     apt-cache madison gitlab-runner
+#     gitlab-runner -version
+#     usermod -aG docker gitlab-runner
+#     usermod -aG sudo gitlab-runner
+#     mkdir -p /home/docker/registry && chmod -R 777 /home/docker/registry && cd /home/docker/registry 
+#     mkdir certs data
+#     apt get update
+#     apt-get install openssl -y
+#   EOF
 
-  tags = {
-    Name = "gr-instance"
-  }
-}
+#   tags = {
+#     Name = "runner-instance"
+#   }
+# }
 
-resource "aws_instance" "be-instance" {
-  ami = var.DEFAULT_AMI
-  instance_type = var.PROVIDER_INSTANCE_TYPE
-  key_name = var.KEY_PAIR
+# resource "aws_instance" "backend-instance" {
+#   ami = var.DEFAULT_AMI
+#   instance_type = var.PROVIDER_INSTANCE_TYPE
+#   key_name = var.KEY_PAIR
 
-  subnet_id = aws_subnet.public-subnet-02.id  
-  vpc_security_group_ids = [aws_security_group.be-sg.id]
-  associate_public_ip_address = true
+#   subnet_id = aws_subnet.public-subnet-02.id  
+#   vpc_security_group_ids = [aws_security_group.backend-sg.id]
+#   associate_public_ip_address = true
 
-  user_data = <<-EOF
-    #!/bin/bash
-    apt-get update
-    apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-    add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-    apt-get update
-    apt-get install docker-ce docker-ce-cli containerd.io -y
-    curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-    chmod +x /usr/local/bin/docker-compose
-  EOF
+#   user_data = <<-EOF
+#     #!/bin/bash
+#     apt-get update
+#     apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
+#     curl -fsSL https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+#     add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+#     apt-get update
+#     apt-get install docker-ce docker-ce-cli containerd.io -y
+#     curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+#     chmod +x /usr/local/bin/docker-compose
+#   EOF
 
-  tags = {
-    Name = "be-instance"
-  }
-}
+#   tags = {
+#     Name = "backend-instance"
+#   }
+# }
